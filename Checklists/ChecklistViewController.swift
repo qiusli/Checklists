cd89a589ad44615512287d4502b26b6cc2b883d3
Qiushi Li <qius.li3@gmail.com>
2015-11-29 02:20:25 -0700

    finished tableview with its data source and delegate methods

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Qiushi Li on 11/29/15.
+//  Copyright © 2015 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS Programming"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer practice"
+        } else {
+            label.text = "Eat ice cream"
+        }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            if cell.accessoryType == .Checkmark {
+                cell.accessoryType = .None
+            } else {
+                cell.accessoryType = .Checkmark
+            }
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+}
+
